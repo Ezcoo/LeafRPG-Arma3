934c74fbcf7294c119a28cbae339db70517d47e3
Ezcoo <ezcoo99@gmail.com>
2023-09-18 20:36:31 +0300

    Add initial project files

---
+++ LeafRPG.VR/functions/CfgFunctions.hpp
@@ -0,0 +1,23 @@
+
+#ifdef DEBUG_ENABLED_FULL
+allowFunctionsRecompile = 1;
+allowFunctionsLog = 1;
+#endif
+
+class CfgFunctions
+{
+
+	class LEAF
+	{
+
+		class core
+		{
+
+			class debug_logContent { file = "functions\core\debug\fn_logContent.sqf"; };
+
+		};
+
+	};
+
+};
+
---
+++ LeafRPG.VR/init.sqf
@@ -0,0 +1,3 @@
+// Logging level. See more info: fn_logContent.sqf
+LeafRPG_logLevel = 0;
+
